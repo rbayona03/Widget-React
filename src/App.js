ce059dcb7322d2f603d24220f9205d8c6d8969fd
Roberto Bayona <rbayona03@gmail.com>
2021-09-17 17:51:22 -0400

    React Navigation and Dynamic page change setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
-
+import Route from "./components/Route";
+import Header from "./components/Header";
 const items = [
   {
     title: "What is React?",
@@ -35,28 +36,29 @@ const options = [
 ];
 
 const App = () => {
-//   const [selected, setSelected] = useState(options[0]);
-//   const [showDropdown, setShowDropdown] = useState(true);
+  const [selected, setSelected] = useState(options[0]);
 
-//   return (
-//     <div>
-//       <button onClick={() => setShowDropdown(!showDropdown)}>
-//         Toggle Dropdown
-//       </button>
-//       {showDropdown ? (
-//         <Dropdown
-//           selected={selected}
-//           onSelectedChange={setSelected}
-//           options={options}
-//         />
-//       ) : null}
-//     </div>
-//   );
-
-    return(
-        <div>
-            <Translate />
-        </div>
-    )
+  return (
+    <div>
+      <Header/>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
 export default App;
